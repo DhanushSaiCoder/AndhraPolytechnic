e3873c0ff512d028801bb041c6e1cb9fa6669d92
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-07-01 23:09:35 +0530

    Add CurrentInfo component and update styles for CommissionerMessage and PrincipalMessage

---
+++ frontend/src/components/homeComponents/CurrentInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import  '../../styles/HomeStyles/CurrentInfo.css';
+
+const CurrentInfo = () => {
+    return (
+        <div className='CurrentInfo'>
+            <h2 className='current-info-title'>CURRENT INFORMATION</h2>
+        </div>
+    );
+}
+
+export default CurrentInfo;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,6 +5,7 @@ import HeroSection from '../components/homeComponents/HeroSection';
 import NoticeBoard from './../components/homeComponents/NoticeBoard';
 import CommissionerMessage from './../components/homeComponents/CommissionerMessage';
 import PrincipalMessage from '../components/homeComponents/PrincipalMessage';
+import CurrentInfo from '../components/homeComponents/CurrentInfo';
 export default function Home() {
   return (
     <div className='Home'>
@@ -12,5 +13,6 @@ export default function Home() {
       <NoticeBoard />
       <CommissionerMessage />
       <PrincipalMessage />
+      <CurrentInfo />
     </div>)
 }
