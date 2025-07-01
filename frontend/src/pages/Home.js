2e8b34ff6e6c005de848a87d6d147e5a5804674e
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-07-01 22:35:25 +0530

    Add CommissionerMessage component with styles and image

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,10 +3,12 @@ import React from 'react';
 import '../styles/HomeStyles/Home.css';
 import HeroSection from '../components/homeComponents/HeroSection';
 import NoticeBoard from './../components/homeComponents/NoticeBoard';
+import CommissionerMessage from './../components/homeComponents/CommissionerMessage';
 export default function Home() {
   return (
     <div className='Home'>
       <HeroSection />
       <NoticeBoard />
+      <CommissionerMessage />
     </div>)
 }
