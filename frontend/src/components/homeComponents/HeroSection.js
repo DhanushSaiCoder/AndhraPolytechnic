21083cd02b4774ca37ee5791fae38e2dbb9bd941
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-07-01 20:07:14 +0530

    Add HeroSection component and update styles for Home page

---
+++ frontend/src/components/homeComponents/HeroSection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../styles/HomeStyles/HeroSection.css'; // Assuming you have a CSS file for styles
+const HeroSection = () => {
+    return (
+        <div className='HeroSection'>
+            Herl
+        </div>
+    );
+}
+
+export default HeroSection;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,10 @@
 //home page
 import React from 'react';
-
+import '../styles/HomeStyles/Home.css';
+import HeroSection from '../components/homeComponents/HeroSection';
 export default function Home() {
-  return <h2>Home Page</h2>;
+  return (
+    <div className='Home'>
+      <HeroSection />
+    </div>)
 }
