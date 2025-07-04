6ca5a5f871661099de31c9ce3f33713f35b06eb5
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-07-04 14:55:05 +0530

    Refactor Map component: remove hardcoded latitude and longitude for cleaner code and update iframe source for accurate location display

--- frontend/src/components/AboutusComponenets/Map.js
+++ frontend/src/components/AboutusComponenets/Map.js
@@ -2,12 +2,9 @@ import React from 'react';
 import '../../styles/AboutUsStyles/Map.css'; // Import the CSS file for styling
 
 const Map = () => {
-    const latitude = 15.9041;
-    const longitude = 80.4669;
-
     return (<div className="map-container">
-        \<iframe
-            src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3826.892!2d${longitude}!3d${latitude}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTXCsDU0JzE0LjgiTiA4MMKwMjgnMDAuOCJF!5e0!3m2!1sen!2sin!4v1635000000000!5m2!1sen!2sin&q=Andhra+Polytechnic+Bapatla`}
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d238.55607592540272!2d82.2323202815691!3d16.93036163169194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a3827eb32268f59%3A0xf1eaf501d757f10e!2sAndhra%20Polytechnic!5e0!3m2!1sen!2sin!4v1751620958521!5m2!1sen!2sin"
             width="100%"
             height="100%"
             style={{ border: 0 }}
