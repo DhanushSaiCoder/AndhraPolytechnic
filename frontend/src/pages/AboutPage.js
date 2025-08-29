bef15a9e90fbcf3e219139783d1d608ba581e578
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-08-29 08:12:22 +0530

    feat: Rename HeroSection components for clarity and fix imports

--- frontend/src/pages/AboutPage.js
+++ frontend/src/pages/AboutPage.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import HeroSection from '../components/AboutUsComponents/HeroSection'
-
+import AboutUsHeroSection from '../components/AboutUsComponents/AboutUsHeroSection'
 import Timeline from '../components/AboutUsComponents/Timeline'
 import Accreditations from '../components/AboutUsComponents/Accreditations'
 import ContactSection from '../components/AboutUsComponents/ContactUs'
@@ -8,7 +7,7 @@ import Achievements from '../components/AboutUsComponents/Achievements'
 const AboutPage = () => {
   return (
     <div className='About'>
-      <HeroSection />
+      <AboutUsHeroSection />
       <Timeline />
       <Accreditations />
       <Achievements />
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,7 @@
 //home page
 import React from 'react';
 import '../styles/HomeStyles/Home.css';
-import HeroSection from '../components/HomeComponents/HeroSection';
+import HomeHeroSection from '../components/HomeComponents/HomeHeroSection';
 import NoticeBoard from './../components/HomeComponents/NoticeBoard';
 import CommissionerMessage from './../components/HomeComponents/CommissionerMessage';
 import PrincipalMessage from '../components/HomeComponents/PrincipalMessage';
@@ -10,12 +10,12 @@ import MessageFromLeaders from './../components/HomeComponents/MessageFromLeader
 import CollegeGallery from './../components/HomeComponents/CollegeGallery';
 import ImageSlider from '../components/HomeComponents/ImageSlider';
 import Departments from '../components/HomeComponents/Departments';
-
+import AboutUsHeroSection from '../components/AboutUsComponents/AboutUsHeroSection'
 
 export default function HomePage() {
   return (
     <div className='Home'>
-      {/* <HeroSection /> */}
+      <AboutUsHeroSection />
       <NoticeBoard />
       <MessageFromLeaders />
       <CurrentInfo />
