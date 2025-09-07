b72497f26642114a6362751c3aa49bd8c5642c96
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-09-07 12:52:44 +0530

    move department

--- frontend/src/data/links.js
+++ frontend/src/data/links.js
@@ -1,6 +1,19 @@
 export const links = [
     { to: '/', label: 'Home', exact: true },
     {
+      to: '/departments',
+      label: 'Departments',
+      subLinks: [
+        { to: '/departments/computer-engineering', label: 'Computer Engineering' },
+        { to: '/departments/electronics-engineering', label: 'Electronics Engineering' },
+        { to: '/departments/electrical-engineering', label: 'Electrical Engineering' },
+        { to: '/departments/mechanical-engineering', label: 'Mechanical Engineering' },
+        { to: '/departments/civil-engineering', label: 'Civil Engineering' },
+        { to: '/departments/architectural-engineering', label: 'Architectural Engineering' },
+        { to: '/departments/automobile-engineering', label: 'Automobile Engineering' },
+      ],
+    },
+    {
       to: '/academics',
       label: 'Academics',
       subLinks: [
@@ -29,19 +42,7 @@ export const links = [
         { to: '/events/cultural', label: 'Cultural Events' },
       ],
     },
-    {
-      to: '/departments',
-      label: 'Departments',
-      subLinks: [
-        { to: '/departments/computer-engineering', label: 'Computer Engineering' },
-        { to: '/departments/electronics-engineering', label: 'Electronics Engineering' },
-        { to: '/departments/electrical-engineering', label: 'Electrical Engineering' },
-        { to: '/departments/mechanical-engineering', label: 'Mechanical Engineering' },
-        { to: '/departments/civil-engineering', label: 'Civil Engineering' },
-        { to: '/departments/architectural-engineering', label: 'Architectural Engineering' },
-        { to: '/departments/automobile-engineering', label: 'Automobile Engineering' },
-      ],
-    },
+    
     { to: '/results', label: 'Results' },
     { to: '/about', label: 'About Us' },
     { to: '/alumni', label: 'Alumni' },
